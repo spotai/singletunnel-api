36b65fb2d3c6bda541266be6bcd57db6a33fc152
Bich Le <bich.le@spotai.co>
2024-07-17 06:21:45 -0700

    initial set of files

---
+++ clntagent/config.go
@@ -0,0 +1,23 @@
+package clntagent
+
+import (
+	api "github.com/spotai/singletunnel-api/coord"
+)
+
+type Config struct {
+	Client          api.ClientInfo
+	CsAddr          string
+	Tls             *Tls `yaml:",omitempty"`
+	Enable          bool
+	OpenVpnPath     string
+	SudoForOpenVpn  bool
+	WireguardKeyDir string // non-empty when wireguard is enabled
+}
+
+type Tls struct {
+	CertFile   string
+	KeyFile    string
+	Insecure   bool   `yaml:",omitempty"`
+	CaFile     string `yaml:",omitempty"`
+	ServerName string `yaml:",omitempty"`
+}
